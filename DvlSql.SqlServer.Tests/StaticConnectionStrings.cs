2b1624e9b7e65cd9e24723952f7d531e461e71ba
DVL_KinoMan <dvali.daviti@gmail.com>
2021-11-14 14:39:34 +0400

    Add project files.

---
+++ DvlSql.SqlServer.Tests/StaticConnectionStrings.cs
@@ -0,0 +1,8 @@
+﻿namespace DvlSql.SqlServer
+{
+    public static class StaticConnectionStrings
+    {
+        public static string ConnectionStringForTest =
+            @"Data Source=(localdb)\MSSQLLocalDB; Initial Catalog=DVL_Test; Connection Timeout=30; Application Name = DVLSqlTest1";
+    }
+}
